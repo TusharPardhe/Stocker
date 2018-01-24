ddf576ad6359fe3407a3ba6e5dcecd83747c16be
Tushar Pardhe <tpardhe@gmail.com>
2018-01-24 14:30:32 +0000

    Added Stock Searching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,18 @@
 class HomeController < ApplicationController
   def index
+    
+      if params[:Id] == ""
+        @nothing = "Hey You Didn't Write Anything"
+      elsif 
+        if params[:Id]
+          begin
+            @stock = StockQuote::Stock.quote(params[:Id])
+          rescue StandardError
+            @error = "Hey I couldn't understand that one"
+          end
+        end
+      end
   end
-  
   def about
   end
 end
